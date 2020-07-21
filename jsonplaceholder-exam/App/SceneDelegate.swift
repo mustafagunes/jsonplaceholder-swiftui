bba5b05a570b790d08acd16471b8dfb506066b00
Mustafa GUNES <gunes149@gmail.com>
2020-07-21 16:38:37 +0300

    edited file and folder structure

---
+++ jsonplaceholder-exam/App/SceneDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  SceneDelegate.swift
+//  jsonplaceholder-exam
+//
+//  Created by Mustafa GUNES on 21.07.2020.
+//  Copyright © 2020 Mustafa GUNES. All rights reserved.
+//
+
+import UIKit
+import SwiftUI
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        let contentView = ContentView()
+
+        // Use a UIHostingController as window root view controller.
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = UIHostingController(rootView: contentView)
+            self.window = window
+            window.makeKeyAndVisible()
+        }
+    }
+}
